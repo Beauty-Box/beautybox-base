7081bf713b135647c7989a0905d3382693870ac4
Dmitry Perfilov <mitka_dimka@live.ru>
2020-10-15 15:55:21 +0400

    fix webpack.legacy.js

--- webpack/modules/webpack.legacy.js
+++ webpack/modules/webpack.legacy.js
@@ -7,7 +7,6 @@ module.exports = {
                 exclude: (file) => /[\\/]node_modules[\\/](!@beautybox)[\\/]/.test(file) && !/\.vue\.js/.test(file),
                 options: {
                     cacheDirectory: true,
-                    envName: 'legacy',
                 },
             },
         ],
