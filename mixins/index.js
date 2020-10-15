a5143973cc9da0f767bc6c477286f7165434bff9
Dmitry Perfilov <mitka_dimka@live.ru>
2020-10-15 16:06:24 +0400

    fix mixins

--- mixins/index.js
+++ mixins/index.js
@@ -1,8 +1,8 @@
-export app from './app.mixin';
+export { useApp } from './app.mixin';
 export checkPermission from './checkPermission';
-export modalProps from './modalProps';
-export notificationProps from './notificationsProps';
+export {modalProps} from './modalProps';
+export { notificationsProps } from './notificationsProps';
 export replaceToQuery from './replaceToQuery';
 export request from './request';
-export scrollControl from './ScrollControl';
+export { scroll as scrollControl} from './ScrollControl';
 export toastMessage from './toastMessage';
