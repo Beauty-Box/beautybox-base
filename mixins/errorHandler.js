6fad734ae886d4cb253fec047c1f00bc2cbe6e60
Dmitry Perfilov <mitka_dimka@live.ru>
2020-11-12 11:02:36 +0400

    hotfix mixins

--- mixins/errorHandler.js
+++ mixins/errorHandler.js
@@ -1,25 +1,26 @@
-const errorHandler = {
-    created() {
-        document.addEventListener('server-error', (e) => {
-            this.$store.dispatch('SERVER_ERROR', e.code);
-        });
-        document.addEventListener('bad-request', (e) => {
-            this.$store.dispatch('BAD_REQUEST', e.code);
-        });
-        document.addEventListener('forbidden', (e) => {
-            this.$store.dispatch('FORBIDDEN', e.code);
-        });
-        document.addEventListener('not-found', (e) => {
-            this.$store.dispatch('NOT_FOUND', e.code);
-        });
-        document.addEventListener('unknown-error', (e) => {
-            this.$store.dispatch('UNKNOWN_ERROR', e.code);
-        });
-        document.addEventListener('clear-error', (e) => {
-            this.$store.dispatch('CLEAR_ERROR', e.code);
-        });
-    },
-};
+export function setErrorHandler(store) {
+    document.addEventListener('server-error', (e) => {
+        store.dispatch('SERVER_ERROR', e.code);
+    });
+    document.addEventListener('bad-request', (e) => {
+        store.dispatch('BAD_REQUEST', e.code);
+    });
+    document.addEventListener('forbidden', (e) => {
+        store.dispatch('FORBIDDEN', e.code);
+    });
+    document.addEventListener('not-found', (e) => {
+        store.dispatch('NOT_FOUND', e.code);
+    });
+    document.addEventListener('unknown-error', (e) => {
+        store.dispatch('UNKNOWN_ERROR', e.code);
+    });
+    document.addEventListener('many-requests', (e) => {
+        store.dispatch('MANY_REQUESTS', e.code);
+    });
+    document.addEventListener('clear-error', (e) => {
+        store.dispatch('CLEAR_ERROR', e.code);
+    });
+}
 
-export { errorHandler };
-export default errorHandler;
+export { setErrorHandler };
+export default setErrorHandler;
