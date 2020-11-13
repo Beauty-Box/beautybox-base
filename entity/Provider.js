5c239fc45fa3f26a16920f235f620cb87c84b66e
Dmitry Perfilov <mitka_dimka@live.ru>
2020-11-13 17:13:22 +0400

    fix entity

--- entity/Provider.js
+++ entity/Provider.js
@@ -4,10 +4,10 @@ export class Provider {
     constructor(config) {
         this.baseUrl = config.BASE_URL;
         this.module = config.module;
-        this.setProvider();
+        this.setProvider(this.baseUrl, this.module, config.token);
     }
 
-    setProvider() {
-        this._provider = new Api(this.baseUrl, this.module);
+    setProvider(baseUrl, module, token) {
+        this._provider = new Api(baseUrl, module, token);
     }
 }
