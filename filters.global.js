d81fb14ce6c7a08be172938099d20bbc6a64c4ec
Dmitry Perfilov <mitka_dimka@live.ru>
2020-11-13 11:34:05 +0400

    fix filters

--- filters.global.js
+++ filters.global.js
@@ -1,20 +1,20 @@
 import Vue from 'vue';
 
 //Date Filters
-import dateFormatFilter from '@beautybox/core/filters/dateFormat.filter.js';
-import dateDayFormat from '@beautybox/core/filters/dateDayFormat.filter.js';
-import dateFullFormatFilter from '@beautybox/core/filters/dateFullFormat.filter';
-import dateMonthShortFormat from '@beautybox/core/filters/dateMonthShortFormat.filter';
+import dateFormatFilter from './filters/dateFormat.filter.js';
+import dateDayFormat from './filters/dateDayFormat.filter.js';
+import dateFullFormatFilter from './filters/dateFullFormat.filter';
+import { dateMonthYearFormat } from './filters/date.filter';
 //Price Filters
-import priceFilter from '@beautybox/core/filters/price.filter.js';
+import priceFilter from './filters/price.filter.js';
 //Time Filters
-import timeFilter from '@beautybox/core/filters/time.filter.js';
+import timeFilter from './filters/time.filter.js';
 
 //Date Filters
 Vue.filter('dateFormat', dateFormatFilter);
 Vue.filter('dateDayFormat', dateDayFormat);
 Vue.filter('dateFullFormat', dateFullFormatFilter);
-Vue.filter('dateMonthShortFormat', dateMonthShortFormat);
+Vue.filter('dateMonthShortFormat', dateMonthYearFormat);
 //Price Filters
 Vue.filter('price', priceFilter);
 //Time Filters
