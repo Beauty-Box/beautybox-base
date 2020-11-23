6dff1ae9b1cc5daf4faf312da9104628e846ba14
Anton Ustinoff <a.a.ustinoff@gmail.com>
2020-11-23 12:43:18 +0400

    fix:(base) add stylelint & eslint & add utility keyReflactor

---
+++ plugins/keyReflactor.js
@@ -0,0 +1,13 @@
+/**
+ * @description Этот метод отвечает за создание объекта с зеркальным ключом и значениями.
+ * а также добавить префикс к значениям, если есть
+ * @param {Array<string>} arr Массив строк
+ * @param {string} prefix префикс
+ * @returns {Object} объект с зеркальными ключами, сгенерированный из массива строк
+ */
+export const reflectKeys = (arr = [], prefix) =>
+    arr.reduce((obj, key) => {
+        obj[key] = prefix ? prefix + ' ' + key : key;
+
+        return obj;
+    }, {});
