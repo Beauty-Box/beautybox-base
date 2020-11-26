0020bce4c269439834a0c88292ae0d52ea72be6f
Dmitry Perfilov <mitka_dimka@live.ru>
2020-11-26 15:22:44 +0400

    add yarn link

--- webpack/middleware/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-    if (!/(\.(?!html)\w+$|__webpack.*)/.test(req.url)) {
-        req.url = '/'; // this would make express-js serve index.html
-    }
-    next();
-};
--- webpack/plugins/svg.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    '404_broken.svg',
-    '404_calendar.svg',
-    '404_staff.svg',
-]
