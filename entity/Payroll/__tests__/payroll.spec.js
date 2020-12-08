b2f33c530f1b10eb516dcc3ee9143622a3c471f7
Dmitry Perfilov <mitka_dimka@live.ru>
2020-12-08 15:36:52 +0400

    add tests

--- entity/Payroll/__tests__/payroll.spec.js
+++ entity/Payroll/__tests__/payroll.spec.js
@@ -1,14 +1,78 @@
 import FixturesSuccess from '../__fixtures__/payrollRulesSuccess.json';
 import FixturesError from '../__fixtures__/payrollRulesError.json';
+import FixturesBrokenData from '../__fixtures__/payrollBrokenData.json';
 import Api from '../../../api';
+jest.mock('../../../api/index.js');
 import { addGetSuccess } from '../../../api/mockHelper';
 
-import { PayrollRules } from '../index';
+import { PayrollRules, Payroll } from '../index';
 
-describe('PayrollRules success', () => {
-    beforeEach(() => {
-        addGetSuccess(Api, FixturesSuccess);
+describe('Payroll instantiation', () => {
+    addGetSuccess(Api, FixturesSuccess);
+
+    it('Должен создать экземпляр', () => {
+        expect(() => new Payroll({ baseUrl: 'test', token: '123' })).not.toThrowError(
+            'Не удается получить токен из входных параметров'
+        );
     });
+
+    it('Должен выдать ошибку что токен не передан', () => {
+        expect(() => new Payroll({ baseUrl: 'test' })).toThrowError(
+            'Не удается получить токен из входных параметров'
+        );
+    });
+
+    it('Должен выдать ошибку что не передан baseUrl', () => {
+        expect(() => new Payroll({ token: '123' })).toThrowError(
+            'Не удается получить baseUrl из входных параметров'
+        );
+    });
+
+    it('Должен выдать ошибку что не переданы параметры для создания экземпляра', () => {
+        expect(() => new Payroll()).toThrowError(
+            'Не переданы входные параметры для создания экземпляра'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры числа вместо объекта', () => {
+        expect(() => new Payroll(123)).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры строки вместо объекта', () => {
+        expect(() => new Payroll('123')).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры false вместо объекта', () => {
+        expect(() => new Payroll(false)).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры true вместо объекта', () => {
+        expect(() => new Payroll(true)).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры массива вместо объекта', () => {
+        expect(() => new Payroll([])).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры null вместо объекта', () => {
+        expect(() => new Payroll(null)).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+});
+
+describe('PayrollRules instantiation', () => {
+    addGetSuccess(Api, FixturesSuccess);
 
     it('Должен создать экземпляр', () => {
         expect(() => new PayrollRules({ baseUrl: 'test', token: '123' })).not.toThrowError(
@@ -34,26 +98,124 @@ describe('PayrollRules success', () => {
         );
     });
 
-    it('Должен создать экземпляр', async () => {
-        expect.assertions(2);
-        const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
-        await payrollRules.getRules();
-        expect(payrollRules.total).toEqual(1);
-        expect(payrollRules.payrollRules).toHaveLength(1);
+    it('Должен выдать ошибку при передаче в параметры числа вместо объекта', () => {
+        expect(() => new PayrollRules(123)).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры строки вместо объекта', () => {
+        expect(() => new PayrollRules('123')).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры false вместо объекта', () => {
+        expect(() => new PayrollRules(false)).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры true вместо объекта', () => {
+        expect(() => new PayrollRules(true)).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры массива вместо объекта', () => {
+        expect(() => new PayrollRules([])).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
+    });
+
+    it('Должен выдать ошибку при передаче в параметры null вместо объекта', () => {
+        expect(() => new PayrollRules(null)).toThrowError(
+            'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }'
+        );
     });
 });
 
-describe('PayrollRules', () => {
-    beforeEach(() => {
-        addGetSuccess(Api, FixturesError);
+describe('PayrollRules getRules', () => {
+    it('Должен получить корректные данные', async () => {
+        addGetSuccess(Api, FixturesSuccess);
+        expect.assertions(2);
+        const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
+        await payrollRules.getRules();
+        expect(payrollRules.total).toEqual(2);
+        expect(payrollRules.payrollRules).toHaveLength(1);
     });
 
-    const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
-
-    it('Должен создать экземпляр', async () => {
+    it('Должен выдать исключение, что сервер вернул ошибку', async () => {
+        addGetSuccess(Api, FixturesError.errorMessages);
         expect.assertions(1);
+        const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
         try {
             await payrollRules.getRules();
+        } catch (e) {
+            expect(e).toEqual('Error');
+        }
+    });
+
+    it('Должен выдать исключение, что сервер вернул неверные данные', async () => {
+        addGetSuccess(Api, FixturesBrokenData.payrollRulesNotArray);
+        expect.assertions(1);
+        const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
+        try {
+            await payrollRules.getRules();
+        } catch (e) {
+            expect(e).toEqual('Устанавливаемое значение не явъяется массивом');
+        }
+    });
+});
+
+describe('PayrollRules addRules', () => {
+    it('Должен выдать исключение, что сервер вернул неверные данные', async () => {
+        addGetSuccess(Api, FixturesSuccess);
+        expect.assertions(1);
+        const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
+        try {
+            await payrollRules.getRules();
+            addGetSuccess(Api, FixturesBrokenData.payrollRulesNotArray);
+            await payrollRules.addRules();
+        } catch (e) {
+            expect(e).toEqual('Устанавливаемое значение не явъяется массивом');
+        }
+    });
+
+    it('Должен выдать исключение, что сервер вернул неверные данные', async () => {
+        addGetSuccess(Api, FixturesSuccess);
+        expect.assertions(1);
+        const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
+        try {
+            await payrollRules.getRules();
+            addGetSuccess(Api, FixturesBrokenData.totalString);
+            await payrollRules.addRules();
+        } catch (e) {
+            expect(e).toEqual('total должен быть целым числом');
+        }
+    });
+});
+
+describe('PayrollRules _testOnError', () => {
+    it('Должен выкидывать исключение', () => {
+        addGetSuccess(Api, FixturesSuccess);
+        expect.assertions(1);
+        const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
+        try {
+            payrollRules.errors = { messages: 'Test' };
+            payrollRules._testOnError();
+        } catch (e) {
+            expect(e).toEqual('Test');
+        }
+    });
+
+    it('Должен выкидывать исключение', () => {
+        addGetSuccess(Api, FixturesSuccess);
+        expect.assertions(1);
+        const payrollRules = new PayrollRules({ baseUrl: 'test', token: '123' });
+        try {
+            payrollRules.errors = { status: 403 };
+            payrollRules._testOnError();
         } catch (e) {
             expect(e).toEqual('Произошла ошибка при получении списка рассчета зарплат');
         }
--- entity/Payroll/index.js
+++ entity/Payroll/index.js
@@ -1,22 +1,23 @@
 import Api from '../../api';
+function testInputData(params) {
+    if (typeof params === 'undefined') {
+        throw 'Не переданы входные параметры для создания экземпляра';
+    } else if (toString.call(params).slice(8, -1) !== 'Object') {
+        throw 'Параметры конструкторы должны быть объектом вида { baseUrl: "http://baseUrl", token: "cmAD3cae3Fafvcae.vfaeFcEEECA3.FEAfaAdawd" }';
+    }
+    if (!params.token) {
+        throw 'Не удается получить токен из входных параметров';
+    }
+    if (!params.baseUrl) {
+        throw 'Не удается получить baseUrl из входных параметров';
+    }
+}
 
 class Payroll {
     constructor(params) {
-        if (!params.token) {
-            throw 'Не удается получить токен из входных параметров';
-        } else {
-            this.token = params.token;
-        }
-        if (!params.baseUrl) {
-            throw 'Не удается получить baseUrl из входных параметров';
-        } else {
-            this.baseUrl = params.baseUrl;
-        }
-        if (!params.id) {
-            throw 'Не удается получить id салона из входных параметров';
-        } else {
-            this.id = params.id;
-        }
+        testInputData(params);
+        this.baseUrl = params.baseUrl;
+        this.token = params.token;
         this.name = '';
         this.value = 0;
         this.salary = 0;
@@ -43,35 +44,73 @@ class Payroll {
 
 class PayrollRules {
     constructor(params) {
-        if (typeof params === 'undefined') {
-            throw 'Не переданы входные параметры для создания экземпляра';
-        }
-        if (!params.token) {
-            throw 'Не удается получить токен из входных параметров';
-        } else {
-            this.token = params.token;
-        }
-        if (!params.baseUrl) {
-            throw 'Не удается получить baseUrl из входных параметров';
-        } else {
-            this.baseUrl = params.baseUrl;
-        }
+        testInputData(params);
+        this.baseUrl = params.baseUrl;
+        this.token = params.token;
         this._provaider = new Api(this.baseUrl, 'crm', this.token);
-
         this.errors = {};
         this.total = 0;
         this.payrollRules = [];
     }
 
+    /** сеттер для добавления новых прав в существующий массив payrollRules */
+    set _pushInPayrollRules(value) {
+        if (!Array.isArray(value)) {
+            throw 'Устанавливаемое значение не явъяется массивом';
+        } else {
+            this.payrollRules.push(...value);
+        }
+    }
+    /** сеттер для добавления присваивания нового массива в payrollRules */
+    set _setPayrollRules(value) {
+        if (!Array.isArray(value)) {
+            throw 'Устанавливаемое значение не явъяется массивом';
+        } else {
+            this.payrollRules = [...value];
+        }
+    }
+    /** сеттер для присваения нового значения total */
+    set _setTotal(value) {
+        if (!Number.isInteger(value)) {
+            throw 'total должен быть целым числом';
+        } else {
+            this.total = value;
+        }
+    }
+
+    /** Публичне методы работы с экземпляром класса */
     async getRules() {
         ({
             errors: this.errors = {},
-            total: this.total = 0,
-            payrollRules: this.payrollRules = [],
+            /** тут используется сеттер, чтобы не писать много проверок в одной функции */
+            total: this._setTotal = 0,
+            payrollRules: this._setPayrollRules = [],
         } = await this._provaider.get('/payroll-rules'));
 
+        this._testOnError();
+    }
+
+    async addRules() {
+        if (this.payrollRules.length < this.total) {
+            ({
+                errors: this.errors = {},
+                /** тут используется сеттер, чтобы не писать много проверок в одной функции */
+                total: this._setTotal = 0,
+                payrollRules: this._pushInPayrollRules = [],
+            } = await this._provaider.get(`/payroll-rules?skip=${this.payrollRules.length}`));
+
+            this._testOnError();
+        }
+    }
+    /** Служебные методы работы с экземпляром класса */
+
+    _testOnError() {
         if (Object.keys(this.errors).length) {
-            throw 'Произошла ошибка при получении списка рассчета зарплат';
+            if (this.errors.messages) {
+                throw this.errors.messages;
+            } else {
+                throw 'Произошла ошибка при получении списка рассчета зарплат';
+            }
         }
     }
 }
