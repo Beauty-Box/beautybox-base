ff3f20a82b2c2796213f51e6fd678c144eff446e
Anton Ustinoff <a.a.ustinoff@gmail.com>
2021-01-25 11:36:50 +0400

    feat:(base) add Order entity

--- entity/Orders/index.js
+++ entity/Orders/index.js
@@ -1,15 +1,78 @@
-import { Provider } from '../Provider';
+import { ProviderClass } from '../../api';
 
-export class Orders extends Provider {
+class Orders extends ProviderClass {
     constructor() {
-        super({ BASE_URL: process.env.BASE_URL, module: 'crm', token: localStorage.getItem('access_token') });
+        super();
+        this._errors = {};
+        this._count = 0;
+        this._items = [];
+        this._status = [
+            {
+                filterText: 'Все',
+                text: 'Все покупки',
+                value: 0,
+            },
+        ];
     }
 
-    // analyze(queryString = '') {
-    //     return this._provider.get(`/analytics/analyze${queryString}`);
-    // }
+    set errors(value) {
+        this._errors = value;
+        if (Object.keys(this._errors).length) {
+            if (this._errors.messages) {
+                throw this._errors.messages;
+            } else {
+                throw 'Произошла ошибка при получении списка рассчета зарплат';
+            }
+        }
+    }
 
-    orders(queryString = '') {
+    set items(value) {
+        if (!Array.isArray(value)) {
+            throw 'Устанавливаемое значение не явъяется массивом';
+        } else {
+            this._items = [...value];
+        }
+    }
+
+    get items() {
+        return this._items;
+    }
+
+    set status(value) {
+        if (!Array.isArray(value)) {
+            throw 'Устанавливаемое значение не явъяется массивом';
+        } else {
+            this._status = [...this._status, ...value];
+        }
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    /** Получение списка покупок */
+    static async getOrders(queryString = '') {
+        const { errors = {}, count = 0, orders = [] } = await Orders._provider.get(
+            `/market/orders${queryString}`
+        );
+        return { errors, count, orders };
+    }
+
+    /** Добавление к списку покупок при скроле */
+    static async getOrdersOnScroll(ordersLength = 0) {
+        const { errors = {}, orders = [] } = await Orders._provider.get(
+            `/market/orders?skip=${ordersLength}`
+        );
+        return { errors, orders };
+    }
+
+    /** Получение списка статусов заказа */
+    static async getOrderStatuses() {
+        const { errors = {}, status = [] } = await Orders._provider.get('/market/status/short');
+        return { errors, status };
+    }
+
+    /*    orders(queryString = '') {
         return this._provider.get(`/market${queryString}`);
     }
 
@@ -35,5 +98,31 @@ export class Orders extends Provider {
 
     itemsInCart() {
         return this._provider.get('/market/products/cart');
+    }*/
+}
+
+class Order extends ProviderClass {
+    constructor() {
+        super();
+        this.address = '';
+        this.date = '';
+        this.deliveryPrice = 0;
+        this.deliveryType = '';
+        this.orderID = 0;
+        this.price = 0;
+        this.products = [];
+        this.sale = 0;
+        this.status = {};
+        this.stock = '';
+        this.total = 0;
+        this.trackNumber = '';
+    }
+
+    static async show(id) {
+        const { errors = {}, ...res } = await Order._provider.get(`/market/orders/${id}`);
+        return { errors, ...res };
     }
 }
+
+export { Orders, Order };
+export default { Orders, Order };
