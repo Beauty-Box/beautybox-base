a843bbb020c5da3e429b86c6e8a2c91fdd8a8d7b
Anton Ustinoff <a.a.ustinoff@gmail.com>
2021-01-28 15:53:45 +0400

    fix:(base) mixins

--- mixins/index.js
+++ mixins/index.js
@@ -1,4 +1,3 @@
 export * from './replaceToQuery';
 export * from './request';
-export * from './ScrollControl';
 export * from './toastMessage';
--- mixins/replaceToQuery.js
+++ mixins/replaceToQuery.js
@@ -5,26 +5,16 @@ const replaceToQuery = {
             return index > -1 ? this.$route.fullPath.slice(index) : '';
         },
         async replaceToQuery(query) {
-            console.log(query);
-            let newQuery = { ...query };
-            console.log(this.$route.query);
-            let test = { ...this.$route.query };
-            Object.keys(newQuery).forEach((item) => {
+            const newQuery = { ...query };
+            const tempQuery = { ...this.$route.query };
+
+            Object.keys(newQuery).forEach(item => {
                 if (!Boolean(newQuery[item])) {
                     delete newQuery[item];
                 }
             });
 
-            // Object.keys(test).forEach((item) => {
-            //     if (parseFloat(test[item]) == test[item] && newQuery[item]) {
-            //         test[item] = parseInt(test[item]);
-            //         newQuery[item] = parseInt(newQuery[item]);
-            //     }
-            // });
-            console.log('test', JSON.stringify(test));
-            console.log('newQuery', JSON.stringify(newQuery));
-            if (JSON.stringify(test) !== JSON.stringify(newQuery)) {
-                console.log('Ура!');
+            if (JSON.stringify(tempQuery) !== JSON.stringify(newQuery)) {
                 await this.$router.replace({ query: newQuery });
                 this.$bus.$emit('route-update');
             }
