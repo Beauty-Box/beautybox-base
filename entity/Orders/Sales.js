c71d769aac150e37370111a585e1fbbd91596f18
Anton Ustinoff <a.a.ustinoff@gmail.com>
2021-02-08 12:41:33 +0400

    fix(entity): Add get QR-code method

--- entity/Orders/Sales.js
+++ entity/Orders/Sales.js
@@ -12,6 +12,18 @@ class Sales extends ProviderClass {
     static async getDiscount(phone) {
         return await Sales._provider.get(`/loyalty/discount?phone=${phone}`);
     }
+
+    /** @description Получение штрихкода скидки
+     *    @function
+     *    @param { String } phone
+     *    @param { Number } size
+     *    @returns {String} Barcode
+     * */
+    static async getDiscountCard(phone = '', size = 3) {
+        return await Sales._provider.get(
+            `/loyalty/discount-card?phone=${phone}&width=${size}&height=${size}`
+        );
+    }
 }
 
 export { Sales };
