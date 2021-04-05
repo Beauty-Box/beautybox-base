fd6ce57a80a70ec371b13d3b9787f45f414eba10
Brix-D <Shlyakhta4@gmail.com>
2021-04-05 17:58:03 +0400

    added tests for person and half client files

--- entity/__tests__/client.spec.js
+++ entity/__tests__/client.spec.js
@@ -1,13 +1,17 @@
 import { describe, it, expect, beforeEach } from '@jest/globals';
 import { Api } from '../../api';
-import { addGetSuccess, addGetError } from '../../api/mockHelper';
+import { addGetSuccess, addGetError, addPostSuccess, addDelete } from '../../api/mockHelper';
 
 jest.mock('../../api/index.js');
 
 import { Client } from '../Clients';
 
 import clientCreateSuccess from '../__fixtures__/clientCreateSuccess.json';
-import clientCreateError from '../__fixtures__/clientCreateError.json';
+import clientShowSuccess from '../__fixtures__/clientShowSuccess.json';
+import clientEditSuccess from '../__fixtures__/clientEditSuccess.json';
+import clientAddPostSuccess from '../__fixtures__/clientAddPostSuccess.json';
+import clientAddPostError from '../__fixtures__/clientAddPostError.json';
+import clientDeleteError from '../__fixtures__/clientDeleteError.json';
 
 const config = {
     baseUrl: 'test',
@@ -55,4 +59,58 @@ describe('client testing', () => {
         }
     });
 
+    it('Должен возвращать json просмотра указанного клиента', async () => {
+        addGetSuccess(Api, clientShowSuccess);
+        try {
+            await client.show();
+            expect(Object.keys(client)).toHaveLength(23);
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+    it('Должен возвращать json редактируемого клиента', async () => {
+        addGetSuccess(Api, clientEditSuccess);
+        try {
+            await client.edit();
+            expect(Object.keys(client)).toHaveLength(20);
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+    it('Должна возвращатся ошибка при апдейте', async () => {
+        addPostSuccess(Api, {});
+        client.clientID = 2422;
+        await client.update();
+        expect(Object.keys(client.errors)).toEqual([]);
+    });
+
+    // it('должен возвращатся clientId созданного клиента', async () => {
+    //     addPostSuccess(Api, clientAddPostSuccess);
+    //     client.phone = '+7 (111) 111-11-11';
+    //     await client.create();
+    //     expect(client.clientID).toEqual(20643);
+    // });
+
+    it('должен возвращатся массив ошибок', async () => {
+        addPostSuccess(Api, clientAddPostError);
+        await client.create();
+        expect(Object.keys(client.errors)).not.toEqual([]);
+    });
+
+    it('клиент должен удалится с ошибкой', async () => {
+        client.clientID = 0;
+        addDelete(Api, {});
+        //let del = jest.fn(() => client.delete);
+        //client.delete();
+        //del();
+        expect(client.delete()).rejects.toMatch('Нельзя удалить не созданого клиента');
+    });
+
+    it('клиент должен удалятся', async () => {
+        client.clientID = 57554;
+        addDelete(Api, {});
+        expect(client.delete()).resolves;
+    });
 });
