b7b9d1af6505dc58a6855eaad54b21b0053028ee
Brix-D <Shlyakhta4@gmail.com>
2022-05-06 17:50:23 +0400

    fix(api): fixed ending

--- helpers/declensions/minuteDeclension.js
+++ helpers/declensions/minuteDeclension.js
@@ -1,15 +1,15 @@
 import wordDeclension from './wordDeclension';
 /**
  * @description minuteDeclension Возвращает правильное скланение слова "минута"
- * 1 минута,
+ * 1 минуту,
  * 2 минуты, 4 минуты,
  * 6 минут, 12 минут,
- * 21 минута
+ * 21 минуту
  * @param { Number } total число от 0 до 99
  * @return { String } Возвращает правильное склонение слова "минута"
  * */
 function minuteDeclension(total) {
-    return wordDeclension(total, { single: 'минута', plural: 'минуты', pluralMoreFive: 'минута' });
+    return wordDeclension(total, { single: 'минуту', plural: 'минуты', pluralMoreFive: 'минут' });
 }
 
 export { minuteDeclension };
