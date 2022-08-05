a6924ff594ae06c3ef13a15e70f984b109dd6ce6
Brix-D <Shlyakhta4@gmail.com>
2022-08-05 18:02:24 +0400

    fix(clients): added helper encodes arrays

--- helpers/__tests__/joinQueryRecursive.spec.js
+++ helpers/__tests__/joinQueryRecursive.spec.js
@@ -21,8 +21,8 @@ describe('regular expression for query params', () => {
             clientBirthdayTo: '2022-08-03',
             clientAgeFrom: '7',
             clientAgeTo: '20',
-            'employees[]': [48683, 36718, 11877, 48557, 31598],
-            'services[]': [25565, 97154],
+            employees: [48683, 36718, 11877, 48557, 31598],
+            services: [],
             profitFrom: '10',
             profitTo: '',
             averageCheckFrom: '',
@@ -34,7 +34,7 @@ describe('regular expression for query params', () => {
     it('should return encoded array', () => {
         console.log(joinQueryRecursive(containArray));
         expect(joinQueryRecursive(containArray)).toEqual(
-            'nameFilter=&customerActivity=permanent&visitPeriodFrom=2022-08-01&visitPeriodTo=2022-08-17&visitExists=false&visitCountFrom=&visitCountTo=&visitOnline=true&visitStatus=cancel&clientGender=2&clientBirthdayFrom=2022-08-03&clientBirthdayTo=2022-08-03&clientAgeFrom=7&clientAgeTo=20&employees[]=48683&employees[]=36718&employees[]=11877&employees[]=48557&employees[]=31598&services[]=25565&services[]=97154&profitFrom=10&profitTo=&averageCheckFrom=&averageCheckTo=10000000'
+            'nameFilter=&customerActivity=permanent&visitPeriodFrom=2022-08-01&visitPeriodTo=2022-08-17&visitExists=false&visitCountFrom=&visitCountTo=&visitOnline=true&visitStatus=cancel&clientGender=2&clientBirthdayFrom=2022-08-03&clientBirthdayTo=2022-08-03&clientAgeFrom=7&clientAgeTo=20&employees[]=48683&employees[]=36718&employees[]=11877&employees[]=48557&employees[]=31598&profitFrom=10&profitTo=&averageCheckFrom=&averageCheckTo=10000000'
         );
     });
     // it('should return match', () => {
--- helpers/joinQueryRecursive.js
+++ helpers/joinQueryRecursive.js
@@ -1,19 +1,26 @@
 export function joinQueryRecursive(params, prefix = '') {
-    const query = Object.keys(params).map((key) => {
-        const value = params[key];
+    const query = Object.keys(params)
+        .map((key) => {
+            const value = params[key];
 
-        if (params.constructor === Array) {
-            key = `${prefix}`;
-        } else if (params.constructor === Object) {
-            key = prefix ? `${prefix}[${key}]` : key;
-        }
+            if (params.constructor === Array) {
+                key = `${prefix}[]`;
+            } else if (params.constructor === Object) {
+                key = prefix ? `${prefix}[${key}]` : key;
+            }
 
-        if (typeof value === 'object') {
-            return joinQueryRecursive(value, key);
-        } else {
-            return `${key}=${encodeURIComponent(value)}`;
-        }
-    });
+            if (typeof value === 'object') {
+                if (Array.isArray(value)) {
+                    if (value.length === 0) {
+                        return '';
+                    }
+                }
+                return joinQueryRecursive(value, key);
+            } else {
+                return `${key}=${encodeURIComponent(value)}`;
+            }
+        })
+        .filter((pair) => pair.length > 0);
 
     return [].concat.apply([], query).join('&');
 }
