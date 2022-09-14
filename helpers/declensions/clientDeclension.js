b24bced2779fdf62d31b7cb7d89822ae129633cd
Brix-D <Shlyakhta4@gmail.com>
2022-09-14 14:43:36 +0400

    fix(mailing): added genitive case to clients declension

--- helpers/declensions/clientDeclension.js
+++ helpers/declensions/clientDeclension.js
@@ -1,16 +1,21 @@
 import plural from 'plural-ru';
 
 /**
- * @description serviceDeclension Возвращает правильное скланение слова "клиент"
+ * @description clientDeclension Возвращает правильное скланение слова "клиент"
  * 1 клиент,
  * 2 клиента, 4 клиента,
  * 6 клиентов, 12 клиентов,
  * 21 клиент
  * @param { Number } total число любое
+ * @param genitive {Boolean} использовать родительный падеж
  * @return { String } Возвращает правильное склонение слова "клиент"
  * */
-function clientDeclension(total) {
-    return plural(total, 'клиент', 'клиента', 'клиентов');
+function clientDeclension(total, genitive = false) {
+    if (genitive) {
+        return plural(total, 'клиента', 'клиентов', 'клиентов');
+    } else {
+        return plural(total, 'клиент', 'клиента', 'клиентов');
+    }
 }
 
 export { clientDeclension };
