718225a34cce2df5903fd1b0b336abbcb9a7eff1
Brix-D <Shlyakhta4@gmail.com>
2023-03-21 16:28:43 +0400

    extended request for employees with filters

--- entity/Employees/index.js
+++ entity/Employees/index.js
@@ -1,4 +1,6 @@
 import { Provider } from '../Provider';
+import { objectToURLParams } from '../../helpers';
+
 
 export class Employees extends Provider {
     constructor() {
@@ -9,7 +11,18 @@ export class Employees extends Provider {
         });
     }
 
-    employees() {
-        return this._provider.get('/employees');
+    employees({ filters = {}, skip = 0 } = {}) {
+        const params = {};
+        if (!!skip) {
+            params.skip = skip;
+        }
+
+        for (const filterKey in filters) {
+            if (!!filters[filterKey]) {
+                params[filterKey] = filters[filterKey];
+            }
+        }
+
+        return this._provider.get(`/employees${objectToURLParams(params)}`);
     }
 }
