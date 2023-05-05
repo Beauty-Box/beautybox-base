edb285ba2968c2c2b1a74012419e3932100d089b
Brix-D <Shlyakhta4@gmail.com>
2023-05-05 12:32:50 +0400

    token

--- utils/getToken.js
+++ utils/getToken.js
@@ -3,7 +3,12 @@ export function getToken() {
     console.log(cookie);
     const token = cookie.find((item) => item.includes('auth._token.laravelJWT'));
     console.log(token);
-    return token;
+    if (!!token) {
+        const tokenParts = token.split('=');
+        const tokenWithoutType = tokenParts[1].replace('Bearer ', '');
+        return tokenWithoutType;
+    }
+    return null;
 }
 
 export default getToken;
