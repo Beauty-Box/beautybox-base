b436493cc1047d95a339ed5d240da869ff5b833a
Brix-D <Shlyakhta4@gmail.com>
2023-05-05 12:59:32 +0400

    fix

--- utils/temporary.js
+++ utils/temporary.js
@@ -36,6 +36,6 @@ function clearAuthCookies() {
     const filteredCookies = cookies.filter((cookie) => cookie.startsWith('auth.'));
     console.log('filteredCookies with auth', filteredCookies);
     filteredCookies.forEach((cookieToDelete) => {
-        document.cookie = cookieToDelete + ';expires=Thu, 01 Jan 1970 00:00:01 GMT';
+        document.cookie = cookieToDelete + '; expires=Thu, 01 Jan 1970 00:00:01 GMT';
     });
 }
