c945d8cbd48e59be18677fa28daa5a0882f88bd3
Brix-D <Shlyakhta4@gmail.com>
2023-05-15 17:07:12 +0400

    fix(helpers): objectToURLParams now can drop empty values

--- helpers/objectToURLParams.js
+++ helpers/objectToURLParams.js
@@ -1,10 +1,19 @@
 /**
  * @description Преобразовывает объект в строку для GET запроса
  * @param {Object} object Объект для преобразования
+ * @param {Boolean} dropEmpty Флаг указывающий следует ли хелперу выкидывать null и undefined
  * @return {String}
  */
-function objectToURLParams(object) {
-    return '?' + new URLSearchParams(object);
+function objectToURLParams(object, dropEmpty = false) {
+    let newObject = {};
+    if (dropEmpty) {
+        newObject = Object.fromEntries(
+            Object.entries(object).filter(([, value]) => value !== null && value !== undefined)
+        );
+    } else {
+        newObject = { ...object };
+    }
+    return '?' + new URLSearchParams(newObject);
 }
 
 export { objectToURLParams };
