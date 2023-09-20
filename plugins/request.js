b505e3cbae8f79743220c9b4cd08cea8455780ae
Brix-D <Shlyakhta4@gmail.com>
2023-09-20 15:22:09 +0400

    feat(plugins): added another plugin for vue 3 request

--- plugins/request.js
+++ plugins/request.js
@@ -5,6 +5,13 @@ export function useApi() {
     return { provider };
 }
 
+export const requestV3 = {
+    install(app, options) {
+        provider = new Api(options.BASE_URL, options.module, options.token, options.secure);
+        app.config.globalProperties.$fetch = provider;
+    },
+};
+
 export default {
     install(Vue, options) {
         provider = new Api(options.BASE_URL, options.module, options.token, options.secure);
